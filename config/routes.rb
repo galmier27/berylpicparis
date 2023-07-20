e36e06119c047f43fee63ec8da59b1103a4dcc3f
galmier forissier <galmierforissier@yahoo.fr>
2023-07-20 11:47:36 +0200

    media querry terminé

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-
+  get "/parcours", to: "pages#parcours"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
